e3bd71f268ea3cb162637d39936cc10d90dcee12
SoulfulGuRa <ahrl1213@naver.com>
2016-10-17 15:36:36 +0900

    [허지훈]Client
    Logo 조작 완성
    start, maptool 들어가는거 구현중

--- Client/Logo.py
+++ Client/Logo.py
@@ -7,9 +7,11 @@ main = None
 start = None
 option = None
 esc = None
+select = None
+PosY = 250
 
 def enter():
-    global main, start, option, esc
+    global main, start, option, esc, select
 
     open_canvas()
 
@@ -17,36 +19,63 @@ def enter():
     start = load_image('Start.png')
     option = load_image('Option.png')
     esc = load_image('Exit.png')
+    select = load_image('Select.png')
 
 def exit():
-    global main, start, option, esc
+    global main, start, option, esc, select
     del(main)
     del(start)
     del(option)
     del(esc)
+    del(select)
 
 def update():
     handle_events()
     delay(0.01)
 
 def draw():
-    global main, start, option, esc
+    global main, start, option, esc, select
+    global PosY
     clear_canvas()
 
     main.draw(400, 300)
     start.draw(330 + 193 / 2, 250)
     option.draw(330 + 134 / 2, 200)
     esc.draw(330 + 77 / 2, 150)
+    select.draw(270, PosY)
 
     update_canvas()
 
 def handle_events():
+    global PosY
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_DOWN:
+            if PosY == 150:
+                PosY = 150
+            else:
+                PosY -= 50
+            print("Down")
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_UP:
+            if PosY == 250:
+                PosY = 250
+            else:
+                PosY += 50
+            print("Up")
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_RETURN:
+            if PosY == 150:
+                game_framework.quit()
+            elif PosY == 200:
+                # MapTool
+                pass
+            elif PosY == 250:
+                # Start
+                pass
+            print("Enter")
 
 def pause(): pass
 
